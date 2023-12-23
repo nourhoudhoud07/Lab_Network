8d34827a86fb5cd6938bc2576cc7431cfdc34374
prateeksheoran <152151910+prateeksheoran@users.noreply.github.com>
2023-12-23 11:04:28 +0100

    gettftp.c

--- gettftp.c
+++ gettftp.c
@@ -1,65 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
 #define MAX_BUFFER_SIZE 516
 
-void error(const char *msg) {
-    perror(msg);
-    exit(1);
+// Function to create a TFTP RRQ packet 
+void create_rrq_packet(char *packet, const char *filename) {
+    
+    packet[0] = 0x00;
+    packet[1] = 0x01;
+    strcpy(&packet[2], filename);
+    strcpy(&packet[2 + strlen(filename) + 1], "octet");
 }
 
-void receiveFile(int sockfd, const char *filename) {
-    FILE *file = fopen(filename, "wb");
-    if (file == NULL) {
-        error("Error opening file for writing");
+// Function to perform TFTP download
+void perform_tftp_download(const char *host, const char *filename) {
+    // Get the server's address using getaddrinfo
+    struct addrinfo hints, *server_info;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC; 
+    hints.ai_socktype = SOCK_DGRAM; 
+
+    int status = getaddrinfo(host, "69", &hints, &server_info);
+    if (status != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit(EXIT_FAILURE);
     }
 
-    while (1) {
-        char buffer[MAX_BUFFER_SIZE];
-        int bytes_received = recv(sockfd, buffer, MAX_BUFFER_SIZE, 0);
-        if (bytes_received < 0) {
-            error("Error receiving data");
-        } else if (bytes_received == 0) {
-            break;  // End of file
+    // Iterate through the list of addresses and reserve a connection socket
+    int sockfd = -1;
+    for (struct addrinfo *p = server_info; p != NULL; p = p->ai_next) {
+        sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (sockfd == -1) {
+            perror("socket error");
+            continue;
         }
 
-        fwrite(buffer, 1, bytes_received, file);
-
-        if (bytes_received < MAX_BUFFER_SIZE) {
-            break;  // Last block received
+        // Build RRQ packet
+        char rrq_packet[MAX_BUFFER_SIZE];
+        create_rrq_packet(rrq_packet, filename);
+			
+        // Send RRQ packet to the server
+        ssize_t bytes_sent = sendto(sockfd, rrq_packet, strlen(rrq_packet), 0, p->ai_addr, p->ai_addrlen);
+        if (bytes_sent == -1) {
+            perror("sendto error");
+            close(sockfd);
+            freeaddrinfo(server_info);
+            exit(EXIT_FAILURE);
         }
+
+        break;
     }
 
-    fclose(file);
+    // Add logic to receive and handle Data (DAT) packets and their acknowledgments (ACK)
+    // ...
+
+    freeaddrinfo(server_info); 
+    close(sockfd); 
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <host> <filename>\n", argv[0]);
-        exit(1);
+        fprintf(stderr, "Usage: %s host file\n", argv[0]);
+        return 1;  
     }
 
     const char *host = argv[1];
-    const char *filename = argv[2];
+    const char *file_to_download = argv[2];
 
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        error("Error opening socket");
-    }
+    perform_tftp_download(host, file_to_download);
 
-    struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(69);
-    server_addr.sin_addr.s_addr = inet_addr(host);
-
-    sendto(sockfd, "get", 3, 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
-
-    receiveFile(sockfd, filename);
-
-    close(sockfd);
-    return 0;
+    return 0;  
 }
